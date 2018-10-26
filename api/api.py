376039fa5c4fd34dfc1090a31f8f41319ddb2c25
michael <michael.wytock@gmail.com>
2018-10-26 17:28:00 -0400

    Removes duplicated TreeHandler in api code.

--- api/api.py
+++ api/api.py
@@ -13,19 +13,6 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("Hello, worlds")
 
 
-class TreeHandler(tornado.web.RequestHandler):
-    #get tree ID, return complexity level, accuracy against training model
-    def get(self, slug):
-        # tree = self.db.get("SELECT * FROM entries WHERE slug = %s", slug)
-        # if not entry: raise tornado.web.HTTPError(404)
-        self.write(slug)
-
-    #classify tree ID - run with test data, return classification and explanation
-    def post(self, slug):
-        test_data = self.get_argument("data")
-        self.write(test_data)
-
-
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),

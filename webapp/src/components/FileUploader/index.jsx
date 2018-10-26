ead234ecded49a60bd10bab0b57b3b2d74f53da1
Erratic Unicorn <david.j.wert@gmail.com>
2018-10-26 15:27:21 -0500

    Begins adding a fetch request

--- webapp/src/components/FileUploader/index.jsx
+++ webapp/src/components/FileUploader/index.jsx
@@ -3,10 +3,21 @@ import Dropzone from 'react-dropzone';
 
 class UploadData extends Component {
 
+    onDrop = file => {
+        console.log(file);
+        fetch("localhost:8888/csv", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            body: file,
+        }).then(response => response.json());
+    }
+
     render() {
         return (
             <div className="file-uploader">
-                <Dropzone onDrop={this.props.onDrop} className="dropzone">
+                <Dropzone onDrop={this.onDrop} className="dropzone">
                     Drag and drop your data file here, or click here to browse...
                 </Dropzone>
             </div>

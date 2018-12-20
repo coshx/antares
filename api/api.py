f71cca11234de521bb8cb68656f6d61e76d126ca
Erratic Unicorn <david.j.wert@gmail.com>
2018-12-20 11:34:30 -0600

    Adds docstring

--- api/api.py
+++ api/api.py
@@ -9,6 +9,7 @@ from registration_handler import RegistrationHandler
 
 # pylint: disable=W0223
 class BaseHandler(tornado.web.RequestHandler):
+    """Sets session token to track current user"""
     def get_current_user(self):
         return self.get_secure_cookie("user")
 

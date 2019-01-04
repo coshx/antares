5daeac0fe055fe487ff5ca2f31f22f8b6013bd66
Erratic Unicorn <david.j.wert@gmail.com>
2019-01-04 11:02:56 -0600

    Removes too long lines

--- api/auth.py
+++ api/auth.py
@@ -10,28 +10,30 @@ def jwt_auth(handler_function):
     def _require_auth(self, *args, **kwargs):
         token = self.request.headers.get_list('Authorization').pop()
         if token:
+            default_error_msg = "Invalid header authorization"
             parts = token.split()
             if parts[0].lower() != 'bearer':
-                throw_authorization_error(self, "Invalid header authorization")
+                throw_authorization_error(self, default_error_msg)
             elif len(parts) == 1:
-                throw_authorization_error(self, "Invalid header authorization")
+                throw_authorization_error(self, default_error_msg)
             elif len(parts) > 2:
-                throw_authorization_error(self, "Invalid header authorization")
+                throw_authorization_error(self, default_error_msg)
 
             jwt_token = parts[1]
             try:
-                decoded_token = jwt.decode(jwt_token, self.settings['jwt_secret'])
+                jwt_secret = self.settings['jwt_secret']
+                decoded_token = jwt.decode(jwt_token, jwt_secret)
                 if user_exists(decoded_token['email']):
                     handler_function(self, *args, **kwargs)
                 else:
-                    throw_authorization_error(self, "Invalid header authorization")
+                    throw_authorization_error(self, default_error_msg)
 
             except jwt.exceptions.DecodeError as error:
                 error_msg = f'Invalid header authorization: {str(error)}'
                 throw_authorization_error(self, error_msg)
 
         else:
-            throw_authorization_error(self, "Invalid header authorization")
+            throw_authorization_error(self, default_error_msg)
     return _require_auth
 
 
